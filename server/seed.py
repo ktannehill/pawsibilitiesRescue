ce061b324c3ddfab66752f291468fde7115f1cb0
Kat Tannehill <katetannehill@gmail.com>
2023-12-04 17:08:28 -0700

    Basic file structure

---
+++ server/seed.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Standard library imports
+from random import randint, choice as rc
+
+# Remote library imports
+from faker import Faker
+
+# Local imports
+from config import app, db
+
+if __name__ == '__main__':
+    fake = Faker()
+    with app.app_context():
+        print("Starting seed...")
+        # Seed code goes here!
